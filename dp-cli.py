327f17d6d6c55b6d83f3faa287bb2337812cacaa
weihong.xu <xuweihong.cn@gmail.com>
2022-08-19 16:47:42 +0800

    refactor: support glob

--- dp-cli.py
+++ dp-cli.py
@@ -4,11 +4,39 @@ import os
 from ase.io import read
 import numpy as np
 import itertools
+from typing import Union
+from glob import glob
 
 
+def add_xyz_to_data_set(in_files: Union[str, list], data_set_dir: str, atoms_kind=0):
 
-def add_xyz_to_data_set(in_file: str, out_dir: str, set_id: int = None, atoms_kind=0):
+    if isinstance(in_files, str):
+        in_files = [in_files]
+    elif not isinstance(in_files, list):
+        raise ValueError('in_files should be string or list of string')
 
+    file_paths = set()
+    for file_path in in_files:
+        file_paths.update(glob(file_path, recursive=True))
+
+    idx_iter = itertools.count(start=0, step=1)
+
+    for file_path in sorted(file_paths):
+        data_set_path = None
+        for idx in idx_iter:
+            set_name = 'set.{}'.format(str(idx).zfill(3))
+            data_set_path = os.path.join(data_set_dir, set_name)
+            try:
+                os.makedirs(data_set_path, exist_ok=False)
+                break
+            except FileExistsError as e:
+                pass
+        convert_xyz_to_training_data(file_path, data_set_path, atoms_kind)
+        print('convert {} to training data {} successfully!'.format(
+            file_path, data_set_path))
+
+
+def convert_xyz_to_training_data(in_file: str, out_dir: str, atoms_kind=0):
     ats = read(in_file, ':')
     force = np.array([np.ravel(at.get_forces()) for at in ats])
     coord = np.array([np.ravel(at.get_positions()) for at in ats])
@@ -22,26 +50,12 @@ def add_xyz_to_data_set(in_file: str, out_dir: str, set_id: int = None, atoms_ki
     sym_dict = dict(zip(symbol_set, range(len(symbol_set))))
     type_raw = [str(sym_dict[specie])
                 for specie in ats[0].get_chemical_symbols()]
-        
-    data_set_path = None
-    
-    # Find a free slot to add new training data
-    for idx in (itertools.count(start=0, step=1) if set_id is None else [set_id]):
-        set_name = 'set.{}'.format(str(idx).zfill(3))
-        data_set_path = os.path.join(out_dir, set_name)
-        try:
-            os.makedirs(data_set_path, exist_ok=False)
-            break
-        except FileExistsError as e :
-            if set_id is not None:
-                raise
-    
+
     for name, data in (('force', force), ('energy', energy), ('coord', coord), ('box', box)):
         out_file = '{}.npy'.format(name)
-        out_file_path = os.path.join(data_set_path, out_file)
+        out_file_path = os.path.join(out_dir, out_file)
         np.save(out_file_path, data)
-
-    raw_file_path = os.path.join(data_set_path, 'type.raw')
+    raw_file_path = os.path.join(out_dir, 'type.raw')
     with open(raw_file_path, 'w') as f:
         f.write(' '.join(type_raw))
 
