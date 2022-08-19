d75ac837a8fded08a7fb226c9c8e390a3063c54e
weihong.xu <xuweihong.cn@gmail.com>
2022-08-19 15:32:51 +0800

    refactor: naming

--- dp-cli.py
+++ dp-cli.py
@@ -6,9 +6,9 @@ import numpy as np
 import itertools
 
 
-def add_xyz_to_training_data_set(in_file: str, out_dir: str, set_id: int = None, atoms_kind=0):
-    ats = read(in_file, ':')
+def add_xyz_to_data_set(in_file: str, out_dir: str, set_id: int = None, atoms_kind=0):
 
+    ats = read(in_file, ':')
     force = np.array([np.ravel(at.get_forces()) for at in ats])
     coord = np.array([np.ravel(at.get_positions()) for at in ats])
     energy = np.array([at.get_potential_energy() for at in ats])
@@ -22,14 +22,14 @@ def add_xyz_to_training_data_set(in_file: str, out_dir: str, set_id: int = None,
     type_raw = [str(sym_dict[specie])
                 for specie in ats[0].get_chemical_symbols()]
         
-    train_data_set_path = None
+    data_set_path = None
     
     # Find a free slot to add new training data
     for idx in (itertools.count(start=0, step=1) if set_id is None else [set_id]):
         set_name = 'set.{}'.format(str(idx).zfill(3))
-        train_data_set_path = os.path.join(out_dir, set_name)
+        data_set_path = os.path.join(out_dir, set_name)
         try:
-            os.makedirs(train_data_set_path, exist_ok=False)
+            os.makedirs(data_set_path, exist_ok=False)
             break
         except FileExistsError as e :
             if set_id is not None:
@@ -37,14 +37,15 @@ def add_xyz_to_training_data_set(in_file: str, out_dir: str, set_id: int = None,
     
     for name, data in (('force', force), ('energy', energy), ('coord', coord), ('box', box)):
         out_file = '{}.npy'.format(name)
-        out_file_path = os.path.join(train_data_set_path, out_file)
+        out_file_path = os.path.join(data_set_path, out_file)
         np.save(out_file_path, data)
 
     raw_file_path = os.path.join(out_dir, 'type.raw')
     with open(raw_file_path, 'w') as f:
         f.write(' '.join(type_raw))
 
+
 if __name__ == '__main__':
     Fire(dict(
-        add_xyz_to_training_data_set=add_xyz_to_training_data_set,
+        add_xyz_to_data_set=add_xyz_to_data_set,
     ))
